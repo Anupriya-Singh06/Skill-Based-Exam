af3e1462ddb9bbe4cc1d2264cb911bfc6fe94408
Anupriya Singh <anupriyasingh423@gmail.com>
2026-02-17 16:05:09 +0530

    program13.py

---
+++ program13.py
@@ -0,0 +1,12 @@
+# Reverse a number using loop
+
+num = int(input("Enter a number: "))
+original = num
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num = num // 10
+
+print("Reverse of the number is:", reverse)
